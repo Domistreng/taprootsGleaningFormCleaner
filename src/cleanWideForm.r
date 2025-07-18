bcef32370ca15fcc2c7497e8d8bc76e038532574
Dominic Sitto <domisrotmg@gmail.com>
2025-07-18 16:59:42 -0400

    Started program for first item

--- src/cleanWideForm.r
+++ src/cleanWideForm.r
@@ -3,3 +3,8 @@ library(dplyr)
 library(tidyverse)
 library(plotmo)
 
+wideForm <-fread('./data/raw/Gleaning Responses Sheet - Form Responses 1.csv')
+View(wideForm)
+
+items1 <- wideForm[,c(1,3,4,5,50,7,8)]
+View(items1)
