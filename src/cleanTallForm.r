73d4bb8b942e2e7a0efff89a598d671cf329f507
Dominic Sitto <domisrotmg@gmail.com>
2025-07-22 12:41:17 -0400

    Wideform Clean

--- src/cleanTallForm.r
+++ src/cleanTallForm.r
@@ -5,14 +5,17 @@ library(plotmo)
 
 wideForm <-fread('./data/raw/Gleaning (Responses) - Form Responses 1.csv')
 
-View(wideForm)
+
 newItems <- wideForm[,c(1, 6, 3, 9, 7, 5, 4)]
 colnames(newItems) = c("Time Logged", "Source", "Name", "Quality", "Price", "Amnt", "Unit")
-View(newItems)
-
-newItems <- filter(newItems, !(is.na(Name) | Name == ""))
 
 
-fwrite(newItems,'./data/processed/WideFormCleaned.csv')
+newItems <- newItems %>%
+    filter(!(is.na(Name) | Name == "")) %>%
+    mutate(Amount = paste0(Amnt, " ", Unit))
 
+newItems$Amnt = NULL
+newItems$Unit = NULL
+
+fwrite(newItems,'./data/processed/TallFormCleaned.csv')
 print("Complete")
